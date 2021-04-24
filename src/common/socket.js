6fd2d4fe7e60812b9d728a41d0b473cb224371a6
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2021-04-24 20:44:29 +0100

    added the chat functionality

--- src/common/socket.js
+++ src/common/socket.js
@@ -1,13 +1,32 @@
 import Chat from '../database/models/chat';
+import User from '../database/models/user';
 
 export const runSocketFunctions = (io) => {
-  io.on('connection', (socket) => {
-    console.log('emited', socket.id);
+  io.on('connection', async (socket) => {
     const id = socket.handshake.query.id;
-    console.log(id, socket.id);
     socket.join(id);
-    // socket.emit('message', 'Chale');
-    socket.on('send message', async ({senderId, message, recepientId }) => {
+
+    socket.on('get user name', async (recepientId) => {
+      const user = await User.findOne({
+        _id: recepientId,
+      }).select('-_id');
+      socket.emit('user mame', user.fullName);
+
+      socket.emit('messages', messages);
+    });
+
+    socket.on('get messages', async (recepientId) => {
+      const messages = await Chat.find({
+        $and: [
+          { $or: [{ senderId: id }, { senderId: recepientId }] },
+          { $or: [{ recepientUserId: recepientId }, { recepientUserId: id }] },
+        ],
+      }).select('-_id');
+
+      socket.emit('messages', messages);
+    });
+
+    socket.on('send message', async ({ senderId, message, recepientId }) => {
       const payload = {
         senderId: senderId,
         recepientUserId: recepientId,
@@ -16,8 +35,6 @@ export const runSocketFunctions = (io) => {
 
       await Chat.create(payload);
       socket.to(recepientId).emit('message', payload);
-      // io.emit('message', body);
-      // socket.broadcast.to(recepient).emit('receive-message');
     });
   });
 };
